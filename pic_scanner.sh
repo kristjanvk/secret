e545202f67e52598b4a5f266783ec1c6c0994484
kristjanvk <36883149+kristjanvk@users.noreply.github.com>
2018-04-24 00:12:10 +0300

    Update pic_scanner.sh

--- pic_scanner.sh
+++ pic_scanner.sh
@@ -1,2 +1,7 @@
 #pildi tuvastamine
 find . -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}' |  xargs -I{} cp  {} /tmp/kuhuiganes
+ja (netist leitud näide) xxd -c1 -p image.jpg | tr "\n" " " | sed -n -e 's/.*\( ff d9 \)\(.*\).*/\2/p' | xxd -r -p
+
+
+Kompineeirituna: (miinus -- tahaks failinime ka )
+find . -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}' |  xargs -I{} xxd -c1 -p {} | tr "\n" " " | sed -n -e 's/.*\( ff d9 \)\(.*\).*/\2/p' | xxd -r -p
